b5adebdbb496fd602d6386c75a807e51722ad262
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2018-06-10 17:00:33 +0300

    Add files via upload

--- vueapp/js/app.js
+++ vueapp/js/app.js
@@ -2,14 +2,21 @@ let vm = new Vue({
     el: "#app",
    data: {
        info: "Hello world!",
-       phrases: ["Hello","I wanna","Best Wishes"]
+       phrases: ["Hello",
+           "I wanna",
+           "Best Wishes",
+           "A",
+           "B",
+           "C"
+       ],
+       minIndex: 0
    },
     methods:{
-        randomElem: function(min,max){
+       randomElem: function(min,max){
             return Math.floor((Math.random()*max)+min);
         },
         generate: function (){
-            this.info = this.phrases[this.randomElem(0,this.phrases.length)];
+            this.info = this.phrases[this.randomElem(this.minIndex,this.phrases.length)];
         }
     }
 });
