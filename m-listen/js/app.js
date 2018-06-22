ca5e59ccaafb3bb456823f090f897bf0549f5493
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2018-06-22 10:17:16 +0300

    Add files via upload

--- m-listen/js/app.js
+++ m-listen/js/app.js
@@ -1,39 +1,44 @@
 Vue.component('player',{
     props: {
-        val: Number,
-        max: Number,
-        counter: Number
-    },
-    computed: {
-        width() {
-            let w = this.val/this.max*100;
-            return{
-                width: w + '%'
-            }
-        }
+        audio: String,
+        volume: String
     },
     template: `
     <div>
-       <audio id="audio">
-             <source src="audio/haskey-piroman.mp3" type="audio/mp3">
+       <audio id="audio" v-bind:volume="volume">
+             <source type="audio/mp3" v-bind:src="audio">
                 Your browser does not support the audio element.
         </audio>
-        </div>`
+     </div>`
 });
 let vm = new Vue({
     el: "#app",
     data: {
-        backgroundAlbum: "url('img/doglife.jpg')" ,
-        audio: String
+        backgroundAlbum: 'img/doglife.jpg' ,
+        audio: "audio/haskey-piroman.mp3",
+        volume: 1.0,
+        statusPlaying: false
     },
     methods:{
         playAudio:function () {
+            this.statusPlaying = true;
+            this.audio = "audio/haskey-piroman.mp3";
+           // document.getElementById("audio").src = this.audio;
             document.getElementById("audio").play();
             this.changeAlbumCover();
         },
+        pauseAudio:function(){
+            this.statusPlaying = false;
+            document.getElementById("audio").pause();
+        },
         changeAlbumCover:function () {
-            document.getElementById("app").style.background = this.backgroundAlbum + "no-repeat center/cover";
-            document.getElementById("player-block").style.background = this.backgroundAlbum + "no-repeat center";
+            //document.getElementById("app").style.background = "url('" + this.backgroundAlbum + "')" + "no-repeat center/cover";
+            document.querySelector("#album-cover>img").src = this.backgroundAlbum;
+        },
+        setVolume: function setVolume(e) {
+            this.volume = e.target.value;
+            console.log(this.volume);
+            document.getElementById('audio').volume = this.volume;
         }
     }
 })
