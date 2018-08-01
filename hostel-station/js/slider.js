904678d746581f488e7cb06b99d13e738f792c2a
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2018-08-01 11:59:14 +0300

    Add files via upload

--- hostel-station/js/slider.js
+++ hostel-station/js/slider.js
@@ -3,25 +3,39 @@
 const nextSlideBtn = document.querySelector("#next-slide");
 const prevSlideBtn = document.querySelector("#prev-slide");
 const slides = document.querySelectorAll(".slider-inner");
-const maxSlide = slides.length-1;
+const maxSlide = document.querySelector("#max-slide");
+const activeSlide = document.querySelector("#active-slide");
+const maxSlideNum = slides.length;
+const sliderControlArea = document.querySelector("#slider-control");
 let activeSlideNum = 0;
+let prevShowedSlide;
+maxSlide.innerHTML = `0${maxSlideNum}`;
+setActiveSlide();
+function setActiveSlide() {
+    activeSlide.innerHTML = `0${activeSlideNum+1}`;
+}
 
 function nextSlide() {
-    slides[activeSlideNum].style.top = "-100vh";
-    activeSlideNum++;
-    slides[activeSlideNum].style.top = "0";
+    prevShowedSlide = slides[activeSlideNum];
+    prevShowedSlide.classList.remove("active");
+    activeSlideNum>=maxSlideNum-1?activeSlideNum=0:activeSlideNum++;
+    console.log(activeSlideNum);
     slides[activeSlideNum].classList.add("active");
-    slides[activeSlideNum - 1].classList.remove("active");
+
+    setActiveSlide();
 }
 
 function prevSlide() {
-    slides[activeSlideNum].style.top = "100vh";
-    activeSlideNum--;
-    slides[activeSlideNum].style.top = "0";
+    prevShowedSlide = slides[activeSlideNum];
+    activeSlideNum<=0?activeSlideNum=maxSlideNum-1:activeSlideNum--;
     slides[activeSlideNum].classList.add("active");
-    slides[activeSlideNum + 1].classList.remove("active");
-
+    prevShowedSlide.classList.remove("active");
+    setActiveSlide();
 }
 
 nextSlideBtn.addEventListener("click", nextSlide);
 prevSlideBtn.addEventListener("click", prevSlide);
+let autoPlaySlide = setInterval(nextSlide,5000);
+// sliderControlArea.addEventListener("mouseover",clearInterval(autoPlaySlide));
+// sliderControlArea.addEventListener("mouseout",setInterval(maxSlide,5000));
+
