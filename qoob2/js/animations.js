99ce6ff85e9b98777f36ba1d6ff5b99d809422f0
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2018-08-15 11:58:13 +0300

    Add files via upload

---
+++ qoob2/js/animations.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const mainPlayBtn = document.getElementById("play-btn");
+const leftMainBlock = document.querySelector(".left-main-block");
+const rightMainBlock = document.querySelector(".right-main-block");
+const rightMainBlockImg = rightMainBlock.querySelector("img");
+const mainContainer = document.querySelector(".main-container");
+let rightMainBlockTexts = rightMainBlock.querySelectorAll("h2");
+const mainContent = document.querySelector("#main-content");
+mainContent.style.display = 'none';
+let activeText = 0;
+let animationStep = 0;
+function FirstStep() {
+    $("#play-btn").fadeOut(100, function () {
+        this.display = "none !important";
+    });
+    leftMainBlock.style.transform = "translate3d(-50vw,0,0)";
+    leftMainBlock.classList.remove("col-5");
+    rightMainBlock.style.left = "0px";
+    rightMainBlockImg.style.transform = "scale(1.4)";
+    setTimeout(function () {
+        rightMainBlock.style.width = "100%";
+    }, 1000);
+    animationStep++;
+    showHeadersText(activeText);
+    setTimeout(
+        function () {
+            let showHeaderInterval = setInterval(function () {
+                if(activeText<=rightMainBlockTexts.length-1){
+                    showHeadersText(activeText);
+                }else {
+                    clearInterval(showHeaderInterval);
+                    secondStep();
+                }
+            },2000);
+        },1000);
+}
+function secondStep() {
+    rightMainBlockImg.style.left = "-10%";
+    rightMainBlockImg.style.transform = "rotate(-45deg)";
+    setTimeout(function () {
+
+        mainContent.style.display = 'block';
+        let offsetTop = $("#main-content").offset().top;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 1500);
+    },1000)
+}
+function showHeadersText(num){
+    if(num===0){
+        rightMainBlockTexts[num].style.display = "block";
+        setTimeout(function () {
+            rightMainBlockTexts[num].classList.add("active");
+        },100);
+
+    }else {
+        rightMainBlockTexts[num-1].classList.remove("active");
+        setTimeout(function () {
+            rightMainBlockTexts[num-1].style.display = "none";
+            rightMainBlockTexts[num].style.display = "block";
+            setTimeout(function () {
+                rightMainBlockTexts[num].classList.add("active");
+            },100);
+        },300);
+    }
+    animationStep++;
+    activeText++;
+}
+mainPlayBtn.addEventListener('click', FirstStep);
+$(window).bind('mousewheel', function (e) {
+    if (e.originalEvent.wheelDelta / 120 > 0) {
+        console.log('scrolling up');
+    }
+    else {
+        switch (animationStep) {
+            case 0 :
+                FirstStep();
+                break;
+        }
+
+
+    }
+});
+
+
