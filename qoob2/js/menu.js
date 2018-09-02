d9503559ea789aa437690074dffc3610126aee8f
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2018-09-02 09:23:19 +0300

    Add files via upload

--- qoob2/js/menu.js
+++ qoob2/js/menu.js
@@ -1,10 +1,28 @@
 "use strict";
-const menuToggler = document.querySelector("#main-hum");
-let isOpened = false;
-
-function toggleMenu() {
-    isOpened = !isOpened;
-    menuToggler.classList.toggle("cross-hum");
+const checkbox = document.getElementById("menu-open");
+const menu = document.querySelector(".menu");
+const closedHeight = 50;
+const openedHeight = 650;
+menu.style.height = `${closedHeight}px`;
+if ("onpropertychange" in checkbox) {
+    // старый IE
+    checkbox.onpropertychange = function() {
+        // проверим имя изменённого свойства
+        if (event.propertyName == "checked") {
+            if(checkbox.checked === true){
+                menu.style.height = `${openedHeight}px`;
+            }else {
+                menu.style.height = `${closedHeight}px`;
+            }
+        }
+    };
+} else {
+    // остальные браузеры
+    checkbox.onchange = function() {
+        if(checkbox.checked === true){
+            menu.style.height = `${openedHeight}px`;
+        }else {
+            menu.style.height = `${closedHeight}px`;
+        }
+    };
 }
-
-menuToggler.addEventListener("click",toggleMenu);
---
+++ qoob2/js/servicesSlider.js
@@ -0,0 +1,95 @@
+const servSubSlider = $('.services-slider');
+const servMainSlider = $('.services-main-slider');
+const nextBtn = $('.right-slick-control');
+const prevBtn = $('.left-slick-control');
+
+
+let activeMainServ = 0;
+let activeSubServ = 0;
+let mainServ = document.querySelectorAll(".services-row");
+let mainServQuantity = mainServ.length;
+
+
+let isMobile = false; //initiate as false
+// device detection
+if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) isMobile = true;
+if (window.innerWidth <= 928){
+    isMobile = true;
+}
+
+servSubSlider.slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    initialSlide: 0,
+    autoplay: false,
+    arrows: false,
+    swipe: false,
+    autoplaySpeed: 2000
+});
+servMainSlider.slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    initialSlide: 0,
+    autoplay: false,
+    arrows: false,
+    swipe: false,
+    autoplaySpeed: 2000
+});
+
+let subServQuantity =  mainServ[servMainSlider.slick('slickCurrentSlide')].querySelectorAll(".sub-serv-inner").length / 2;
+console.log(subServQuantity);
+nextBtn.click(function () {
+    activeMainServ = servMainSlider.slick('slickCurrentSlide');
+    activeSubServ = servSubSlider.slick('slickCurrentSlide');
+    subServQuantity = mainServ[servMainSlider.slick('slickCurrentSlide')].querySelectorAll(".sub-serv-inner").length / 2 - 1;
+    if (activeSubServ >= subServQuantity - 1) {
+        if (activeMainServ === mainServQuantity - 1) {
+            servMainSlider.slick('slickGoTo', 0);
+            servSubSlider.slick('slickGoTo', 0);
+        }else {
+            servSubSlider.slick('slickGoTo', 0);
+            servMainSlider.slick('slickNext');
+        }
+    } else {
+        servSubSlider.slick('slickNext');
+    }
+});
+prevBtn.click(function () {
+    activeMainServ = servMainSlider.slick('slickCurrentSlide');
+    activeSubServ = servSubSlider.slick('slickCurrentSlide');
+    if (activeSubServ <= 0) {
+        if (activeMainServ === 0) {
+            servMainSlider.slick('slickGoTo', mainServQuantity - 1);
+            subServQuantity = mainServ[servMainSlider.slick('slickCurrentSlide')].querySelectorAll(".sub-serv-inner").length / 2 - 1;
+            console.log(subServQuantity);
+            if(subServQuantity>1){
+                servSubSlider.slick('slickGoTo', subServQuantity - 1);
+            }
+        }else {
+            servMainSlider.slick('slickPrev');
+            subServQuantity = mainServ[servMainSlider.slick('slickCurrentSlide')].querySelectorAll(".sub-serv-inner").length / 2 - 1;
+            console.log(subServQuantity);
+            if(subServQuantity>1){
+                servSubSlider.slick('slickGoTo', subServQuantity - 1);
+            }
+        }
+        // servMainSlider.slick('slickGoTo', mainServQuantity - 1);
+        // servSubSlider.slick('slickGoTo', subServQuantity - 1);
+    } else {
+        servSubSlider.slick('slickPrev');
+    }
+});
+window.onresize = function () {
+    if(isMobile){
+        servSubSlider.slick('unslick');
+        servMainSlider.slick('unslick');
+        // document.querySelector(".services-main-slider").classList.remove("col-10");
+        // document.querySelector(".services-main-slider").classList.add("col-12");
+        console.log("unslicked");
+        document.querySelector(".right-slick-control").style.display = "none";
+        document.querySelector(".left-slick-control").style.display = "none";
+    }
+};
