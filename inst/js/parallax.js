a5bea290af4cb36506d2d144205022aebec68570
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2019-03-09 19:38:25 +0200

    Add files via upload

--- inst/js/parallax.js
+++ inst/js/parallax.js
@@ -1,18 +1,18 @@
 function castParallax() {
 
-    var opThresh = 350;
-    var opFactor = 750;
+    let opThresh = 350;
+    let opFactor = 750;
 
-    window.addEventListener("scroll", function(event){
+    window.addEventListener("scroll", function (event) {
 
-        var top = this.pageYOffset;
+        let top = this.pageYOffset;
 
-        var layers = document.getElementsByClassName("parallax");
-        var layer, speed, yPos;
-        for (var i = 0; i < layers.length; i++) {
+        let layers = document.getElementsByClassName("parallax");
+        let layer, speed, yPos;
+        for (let i = 0; i < layers.length; i++) {
             layer = layers[i];
             speed = layer.getAttribute('data-speed');
-            var yPos = -(top * speed / 100);
+            let yPos = -(top * speed / 100);
             layer.setAttribute('style', 'transform: translate(0px, ' + yPos + 'px)');
 
         }
@@ -20,3 +20,17 @@ function castParallax() {
 }
 
 document.body.onload = castParallax();
+let layer1 = document.querySelector('.money-wrapper > img');
+let layer2 = document.querySelector('.money-text-wrapper');
+window.addEventListener('scroll',(e)=>{
+    let top = document.getElementById('total').offsetTop - this.pageYOffset;
+
+
+    let speed1,speed2, yPos1,yPos2;
+    speed1 = 40;
+    yPos1 = (top * speed1 / 100);
+    speed2 = 5;
+    yPos2 = -(top * speed2 / 100);
+    layer1.setAttribute('style', 'transform: translate(0px, ' + yPos1 + 'px)');
+    layer2.setAttribute('style', 'transform: translate(0px, ' + yPos2 + 'px)');
+});
