5f12e3d278cdf426f44e10f258b50bd31ad61460
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2020-02-01 19:22:10 +0200

    Add files via upload

--- food_art/js/menu.js
+++ food_art/js/menu.js
@@ -5,3 +5,14 @@ for (let i = 0; i < menuToggler.length; i++) {
         menu.classList.toggle("navigation__menu--active");
     });
 }
+$(function() {
+    $('#lang').selectric();
+});
+const languageToggler = document.querySelectorAll(".language-toggler");
+const languagePopUp = document.querySelector(".language-popup");
+for (let i = 0; i < languageToggler.length; i++) {
+    languageToggler[i].addEventListener("click", () => {
+        languagePopUp.classList.toggle("language-popup--active");
+    });
+}
+
