c9d00733b133d5df55389d0612f9d44adf0e77b8
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2020-08-27 14:15:21 +0300

    Add files via upload

---
+++ belkaPost/precache-manifest.69f3831e0eed7ac5c95c9e25172ea61b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b92846403e3d8ea5965a6a2cf250e80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0de816ff4c876ceae0b8",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "ed7d6567848d209973fc",
+    "url": "/static/css/main.7849967d.chunk.css"
+  },
+  {
+    "revision": "0de816ff4c876ceae0b8",
+    "url": "/static/js/2.4402f30f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4402f30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7d6567848d209973fc",
+    "url": "/static/js/main.da5ca8ba.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "09df87d63f327788d25a727d0e6c8ccc",
+    "url": "/static/media/05.09df87d6.png"
+  },
+  {
+    "revision": "0fe8b39328b4fa6490cc5916df0752e5",
+    "url": "/static/media/GothaProBla.0fe8b393.otf"
+  },
+  {
+    "revision": "3f9c5a2b33fd28799ac8e55c093a4531",
+    "url": "/static/media/GothaProBol.3f9c5a2b.otf"
+  },
+  {
+    "revision": "aab7d221c447103e2454922cae29a3d7",
+    "url": "/static/media/GothaProLig.aab7d221.otf"
+  },
+  {
+    "revision": "dd3d9ca53a246950706e94726bb1403e",
+    "url": "/static/media/GothaProMed.dd3d9ca5.otf"
+  },
+  {
+    "revision": "aafeb23794c0546aa78e6d11e49ef908",
+    "url": "/static/media/GothaProReg.aafeb237.otf"
+  },
+  {
+    "revision": "56adb4396b75ba7fb1068de290d3c68b",
+    "url": "/static/media/boxes.56adb439.svg"
+  },
+  {
+    "revision": "b876aae981d7b04fdf592c5a05b3f055",
+    "url": "/static/media/map-of-world.b876aae9.png"
+  },
+  {
+    "revision": "a559ca5e971ca0fb5d2c5c372daa0c16",
+    "url": "/static/media/opportunity_2.a559ca5e.png"
+  },
+  {
+    "revision": "22c14f5ac054be5956c74c9bc4b3b3d8",
+    "url": "/static/media/opportunity_4.22c14f5a.png"
+  },
+  {
+    "revision": "09df87d63f327788d25a727d0e6c8ccc",
+    "url": "/static/media/opportunity_5.09df87d6.png"
+  },
+  {
+    "revision": "e03cad667c36c410e3cd5b802f4a94ea",
+    "url": "/static/media/opportunity_6.e03cad66.png"
+  },
+  {
+    "revision": "f6d26c5dd1d24cd480aafc7fa4d44ff3",
+    "url": "/static/media/screenshot-1.f6d26c5d.png"
+  },
+  {
+    "revision": "40b3cc48f4fd498793112d6bab8c0c3e",
+    "url": "/static/media/screenshot-2.40b3cc48.png"
+  }
+]);
