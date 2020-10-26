86c6a87a07749909430d86200a12d876a755ce24
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2020-10-26 11:10:23 +0200

    Add files via upload

---
+++ asimica/js/team.js
@@ -0,0 +1,148 @@
+const personArray = [{
+        name: "Nikolai Mushnikov ",
+        position: "Co-Founder and CEO of AsimicA",
+        photo: "person_1.png",
+        education: {
+            title: "EDUCATION/TRAINING",
+            text: `Saint-Petersburg State University, Russia, B.S. 05/2008, Yeast Genetics
+            Saint-Petersburg State University, Russia, M.S., 12/2010, Cytology and Histology
+            University of Wyoming, WY, USA, Ph.D. 12/2019, Molecular Biology, Microbiology
+            CEO and co-founder of AsimicA: 2018 - present`,
+        },
+        blocks: [{
+                title: "PERSONAL STATEMENT",
+                text: `The goal of my PhD thesis project was to develop synthetic genetic circuitry for controlling asymmetric cell division and cell differentiation in E. coli. My discoveries led to a prominent publication in Nature Chemical Biology1, 2, 3, a new patent-pending invention called Microbial Stem Cell Technology (MiST)4, and the founding of a small business, called AsimicA LLC, whose goal is to apply that technology in bringing transformational change across multiple biomanufacturing industries. In the first years of my thesis project, I designed, carried out, and interpreted biological experiments, formed productive collaboration with other scientists, presented my research at multiple scientific conferences, and trained new students in laboratory practices. Over the past two years, I have maintained these activities and at the same time worked towards establishing myself as an upcoming biotech entrepreneur. To this end, I have obtained patent protection for MiST, my intellectual property, I have incorporated a small business for which I am lead scientist and chief officer, I have obtained some seed funding to start that business, and I have put special effort into making connections that will help my small business develop and grow. These connections include mentors and advisors at the Wyoming Technology Business Center (WTBC), Wyoming Small Business Development Center (SBDC), and a number of contacts in academic fermentation science, synthetic biology, and bioproduction industries. Last year I competed in the “Fisher Innovation Challenge”, a competition between small business student teams in Wyoming, and reached the level of finalist. I also won competitive funding for AsmicA through the Wyoming SBIR/STTR Initiative, which provided an SBIR Phase 0-00 grant to support travel, technical training, and business development networking. I look forward to following my entrepreneurial goals with future small business funding, with the mid-term goal of commercializing AsimicA technology and expertise by forming partnerships with large biomanufacturing companies.
+​1. Mushnikov, N. V., Fomicheva, A., Gomelsky, M. & Bowman, G. R. “Inducible asymmetric cell division and programmable cell differentiation in bacterium“. Nat. Chem. Biol. (2019). doi:10.1038/s41589-019-0340-4.
+2. Ma, H. R. and You, L. “Polar-opposite fates”, Nature Chemical Biology (2019). Springer US. doi: 10.1038/s41589-019-0337-z.
+3. York, A. “Differentiating bacteria”, Nature Reviews Microbiology, 924, p. 41579, (2019). doi: 10.1038/s41579-019-0261-y.
+(‘2’ and ‘3’ are the news articles, which discuss the Mushnikov et al. publication and its broader implications).
+4. Provisional (Submitted April 2018) and non-provisional (Submitted April 2019) patent submissions. “Microbial Stem Cell Technology” non-provisional patent is currently under review at the U.S. Patent and Trademark Office (EFS id: 35822161).`,
+            },
+            {
+                title: "PERSONAL STATEMENT 2",
+                text: `The goal of my PhD thesis project was to develop synthetic genetic circuitry for controlling asymmetric cell division and cell differentiation in E. coli. My discoveries led to a prominent publication in Nature Chemical Biology1, 2, 3, a new patent-pending invention called Microbial Stem Cell Technology (MiST)4, and the founding of a small business, called AsimicA LLC, whose goal is to apply that technology in bringing transformational change across multiple biomanufacturing industries. In the first years of my thesis project, I designed, carried out, and interpreted biological experiments, formed productive collaboration with other scientists, presented my research at multiple scientific conferences, and trained new students in laboratory practices. Over the past two years, I have maintained these activities and at the same time worked towards establishing myself as an upcoming biotech entrepreneur. To this end, I have obtained patent protection for MiST, my intellectual property, I have incorporated a small business for which I am lead scientist and chief officer, I have obtained some seed funding to start that business, and I have put special effort into making connections that will help my small business develop and grow. These connections include mentors and advisors at the Wyoming Technology Business Center (WTBC), Wyoming Small Business Development Center (SBDC), and a number of contacts in academic fermentation science, synthetic biology, and bioproduction industries. Last year I competed in the “Fisher Innovation Challenge”, a competition between small business student teams in Wyoming, and reached the level of finalist. I also won competitive funding for AsmicA through the Wyoming SBIR/STTR Initiative, which provided an SBIR Phase 0-00 grant to support travel, technical training, and business development networking. I look forward to following my entrepreneurial goals with future small business funding, with the mid-term goal of commercializing AsimicA technology and expertise by forming partnerships with large biomanufacturing companies.
+​1. Mushnikov, N. V., Fomicheva, A., Gomelsky, M. & Bowman, G. R. “Inducible asymmetric cell division and programmable cell differentiation in bacterium“. Nat. Chem. Biol. (2019). doi:10.1038/s41589-019-0340-4.
+2. Ma, H. R. and You, L. “Polar-opposite fates”, Nature Chemical Biology (2019). Springer US. doi: 10.1038/s41589-019-0337-z.
+3. York, A. “Differentiating bacteria”, Nature Reviews Microbiology, 924, p. 41579, (2019). doi: 10.1038/s41579-019-0261-y.
+(‘2’ and ‘3’ are the news articles, which discuss the Mushnikov et al. publication and its broader implications).
+4. Provisional (Submitted April 2018) and non-provisional (Submitted April 2019) patent submissions. “Microbial Stem Cell Technology” non-provisional patent is currently under review at the U.S. Patent and Trademark Office (EFS id: 35822161).`,
+            },
+        ],
+    },
+    {
+        name: "Nikolai Mushnikov 2",
+        position: "Co-Founder and CEO of AsimicA",
+        photo: "person_2.png",
+        education: {
+            title: "EDUCATION/TRAINING",
+            text: `Saint-Petersburg State University, Russia, B.S. 05/2008, Yeast Genetics
+            Saint-Petersburg State University, Russia, M.S., 12/2010, Cytology and Histology
+            University of Wyoming, WY, USA, Ph.D. 12/2019, Molecular Biology, Microbiology
+            CEO and co-founder of AsimicA: 2018 - present`,
+        },
+        blocks: [{
+            title: "PERSONAL STATEMENT",
+            text: `The goal of my PhD thesis project was to develop synthetic genetic circuitry for controlling asymmetric cell division and cell differentiation in E. coli. My discoveries led to a prominent publication in Nature Chemical Biology1, 2, 3, a new patent-pending invention called Microbial Stem Cell Technology (MiST)4, and the founding of a small business, called AsimicA LLC, whose goal is to apply that technology in bringing transformational change across multiple biomanufacturing industries. In the first years of my thesis project, I designed, carried out, and interpreted biological experiments, formed productive collaboration with other scientists, presented my research at multiple scientific conferences, and trained new students in laboratory practices. Over the past two years, I have maintained these activities and at the same time worked towards establishing myself as an upcoming biotech entrepreneur. To this end, I have obtained patent protection for MiST, my intellectual property, I have incorporated a small business for which I am lead scientist and chief officer, I have obtained some seed funding to start that business, and I have put special effort into making connections that will help my small business develop and grow. These connections include mentors and advisors at the Wyoming Technology Business Center (WTBC), Wyoming Small Business Development Center (SBDC), and a number of contacts in academic fermentation science, synthetic biology, and bioproduction industries. Last year I competed in the “Fisher Innovation Challenge”, a competition between small business student teams in Wyoming, and reached the level of finalist. I also won competitive funding for AsmicA through the Wyoming SBIR/STTR Initiative, which provided an SBIR Phase 0-00 grant to support travel, technical training, and business development networking. I look forward to following my entrepreneurial goals with future small business funding, with the mid-term goal of commercializing AsimicA technology and expertise by forming partnerships with large biomanufacturing companies.
+​1. Mushnikov, N. V., Fomicheva, A., Gomelsky, M. & Bowman, G. R. “Inducible asymmetric cell division and programmable cell differentiation in bacterium“. Nat. Chem. Biol. (2019). doi:10.1038/s41589-019-0340-4.
+2. Ma, H. R. and You, L. “Polar-opposite fates”, Nature Chemical Biology (2019). Springer US. doi: 10.1038/s41589-019-0337-z.
+3. York, A. “Differentiating bacteria”, Nature Reviews Microbiology, 924, p. 41579, (2019). doi: 10.1038/s41579-019-0261-y.
+(‘2’ and ‘3’ are the news articles, which discuss the Mushnikov et al. publication and its broader implications).
+4. Provisional (Submitted April 2018) and non-provisional (Submitted April 2019) patent submissions. “Microbial Stem Cell Technology” non-provisional patent is currently under review at the U.S. Patent and Trademark Office (EFS id: 35822161).`,
+        }, ],
+    },
+    {
+        name: "Nikolai Mushnikov 3",
+        position: "Co-Founder and CEO of AsimicA",
+        photo: "person_3.png",
+        education: {
+            title: "EDUCATION/TRAINING",
+            text: `Saint-Petersburg State University, Russia, B.S. 05/2008, Yeast Genetics
+            Saint-Petersburg State University, Russia, M.S., 12/2010, Cytology and Histology
+            University of Wyoming, WY, USA, Ph.D. 12/2019, Molecular Biology, Microbiology
+            CEO and co-founder of AsimicA: 2018 - present`,
+        },
+        blocks: [{
+            title: "PERSONAL STATEMENT",
+            text: `The goal of my PhD thesis project was to develop synthetic genetic circuitry for controlling asymmetric cell division and cell differentiation in E. coli. My discoveries led to a prominent publication in Nature Chemical Biology1, 2, 3, a new patent-pending invention called Microbial Stem Cell Technology (MiST)4, and the founding of a small business, called AsimicA LLC, whose goal is to apply that technology in bringing transformational change across multiple biomanufacturing industries. In the first years of my thesis project, I designed, carried out, and interpreted biological experiments, formed productive collaboration with other scientists, presented my research at multiple scientific conferences, and trained new students in laboratory practices. Over the past two years, I have maintained these activities and at the same time worked towards establishing myself as an upcoming biotech entrepreneur. To this end, I have obtained patent protection for MiST, my intellectual property, I have incorporated a small business for which I am lead scientist and chief officer, I have obtained some seed funding to start that business, and I have put special effort into making connections that will help my small business develop and grow. These connections include mentors and advisors at the Wyoming Technology Business Center (WTBC), Wyoming Small Business Development Center (SBDC), and a number of contacts in academic fermentation science, synthetic biology, and bioproduction industries. Last year I competed in the “Fisher Innovation Challenge”, a competition between small business student teams in Wyoming, and reached the level of finalist. I also won competitive funding for AsmicA through the Wyoming SBIR/STTR Initiative, which provided an SBIR Phase 0-00 grant to support travel, technical training, and business development networking. I look forward to following my entrepreneurial goals with future small business funding, with the mid-term goal of commercializing AsimicA technology and expertise by forming partnerships with large biomanufacturing companies.
+​1. Mushnikov, N. V., Fomicheva, A., Gomelsky, M. & Bowman, G. R. “Inducible asymmetric cell division and programmable cell differentiation in bacterium“. Nat. Chem. Biol. (2019). doi:10.1038/s41589-019-0340-4.
+2. Ma, H. R. and You, L. “Polar-opposite fates”, Nature Chemical Biology (2019). Springer US. doi: 10.1038/s41589-019-0337-z.
+3. York, A. “Differentiating bacteria”, Nature Reviews Microbiology, 924, p. 41579, (2019). doi: 10.1038/s41579-019-0261-y.
+(‘2’ and ‘3’ are the news articles, which discuss the Mushnikov et al. publication and its broader implications).
+4. Provisional (Submitted April 2018) and non-provisional (Submitted April 2019) patent submissions. “Microbial Stem Cell Technology” non-provisional patent is currently under review at the U.S. Patent and Trademark Office (EFS id: 35822161).`,
+        }, ],
+    },
+];
+let personInfo = {};
+const personInfoWrapper = document.querySelector("#person-info");
+const personInfoElement = document.querySelector(
+    "#person-info .person-info__content .person-info__inner"
+);
+const fillPersonInfo = (id = 0) => {
+    personInfo = personArray[id];
+
+    /* main info */
+    const mainInfo = document.querySelector(".person-info__main-info");
+    mainInfo.innerHTML = "";
+    const p = document.createElement("p");
+    const div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+    /* Name */
+    const name = document.createElement("span");
+    name.classList = "headline-5 text-bold text-uppercase text--gradient";
+    name.innerText = personInfo.name;
+    div.appendChild(name);
+    /* position */
+    const position = document.createElement("span");
+    position.classList = "mt-3";
+    position.innerText = personInfo.position;
+    div.appendChild(position);
+
+    p.classList = "paragraph my-3";
+    p.style.fontWeight = "700";
+    p.innerText = personInfo.education.title;
+    div.appendChild(p);
+
+    personInfo.education.text.split("\n").map((paragraph) => {
+        const innerP = document.createElement("p");
+        innerP.innerText = paragraph;
+        div.appendChild(innerP);
+    });
+    mainInfo.appendChild(div);
+    var img = new Image();
+    img.classList = "img-fluid b-r-30";
+    img.src = `./img/${personInfo.photo}`;
+    mainInfo.appendChild(img);
+    // mainInfo.innerText = personInfo.photo;
+
+    /* blocks */
+    personInfo.blocks.map((block) => {
+        const p = document.createElement("p");
+        p.classList = "paragraph my-3";
+        p.style.fontWeight = "700";
+        p.innerText = block.title;
+        personInfoElement.appendChild(p);
+        block.text.split("\n").map((paragraph) => {
+            const innerP = document.createElement("p");
+            innerP.innerText = paragraph;
+            personInfoElement.appendChild(innerP);
+        });
+    });
+};
+
+const closeButton = document.getElementById("person-info__close");
+closeButton.addEventListener("click", (e) => {
+    personInfoWrapper.classList.toggle("person-info--active");
+});
+
+const aboutButtons = document.querySelectorAll(".about-button");
+for (let i = 0; i < aboutButtons.length; i++) {
+    aboutButtons[i].addEventListener("click", (e) => {
+        // console.log()
+        if (e.target.dataset.personId) {
+            fillPersonInfo(e.target.dataset.personId);
+            personInfoWrapper.classList.toggle("person-info--active");
+        }
+        // personInfoWrapper.classList.toggle("person-info--active")
+    });
+}
